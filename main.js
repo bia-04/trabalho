95d1b2286c7c2e8816f5062f8feb3e03f36610e9
bia-04 <114154679+bia-04@users.noreply.github.com>
2023-09-22 09:05:25 -0300

    ghfgg

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-document.querySelector('.tecla_pom');
-document.querySelector('#som_tecla_pom').play();
+function contar(idAudio) {
+    document.querySelector(idAudio).play();
+} 
+ 
+const lista = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < lista.length) {
+    lista[contador].onclick = function () {
+        tocar('#som_tecla_toim');
+    }
+    const classe = lista[contador].classsList[1];
+    console.log(classe);
+    contador = contador + 1;
+    console.log (contador);
+}
