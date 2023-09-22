97f37ab65fd05b428e5d1bb7e045ebd4cb5c3383
bia-04 <114154679+bia-04@users.noreply.github.com>
2023-09-22 09:11:27 -0300

    sdfsfg

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let contador = 0;
 
 while (contador < lista.length) {
     lista[contador].onclick = function () {
-        tocar('#som_tecla_toim');
+        contar('#som_tecla_toim');
     }
     const classe = lista[contador].classList[1];
     console.log(classe);
