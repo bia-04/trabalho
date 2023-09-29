32773492ba73e87a4ab3f86d7e46eac44bcb623c
bia-04 <114154679+bia-04@users.noreply.github.com>
2023-09-29 07:52:27 -0300

    dgdffd

--- main.js
+++ main.js
@@ -7,13 +7,18 @@ const lista = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < lista.length) {
+    const classe = lista[contador].classList[1];
+    console.log(classe);
+
+    const idAudio = `#som_${classe}`;
+    console.log(idAudio);
+    
     lista[contador].onclick = function () {
         contar('#som_tecla_toim');
     }
-    const classe = lista[contador].classList[1];
-    console.log(classe);
+   
+
     contador = contador + 1;
     console.log (contador);
-    const idAudio = `#som_${classe}`;
-    console.log(idAudio);
+   
 }
